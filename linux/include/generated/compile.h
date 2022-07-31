3862d0b47f4f3deec937fbb01d7e4be40c4fe4fd
916BGAI <916772719@qq.com>
2022-08-01 00:47:04 +0800

    2th commit

---
+++ linux/include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 46 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#46 Sun Jul 31 22:40:07 CST 2022"
+#define LINUX_COMPILE_BY "kjlsai"
+#define LINUX_COMPILE_HOST "Arch"
+#define LINUX_COMPILER "gcc version 11.3.0 (Buildroot 2022.05)"
